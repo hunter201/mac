75e8aa6dcdd9dfef78b83b254f1161102d478e30
Rad <ggggg@MacBook-Pro-ggggg.local>
2021-11-17 00:21:32 +0400

    The header ended, Main begin

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/app.css">
-    <script defer src="../node_modules/@fortawesome/fontawesome-free/js/solid.js"></script>
+    <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.css">
     <title>Welcome</title>
 </head>
 
@@ -16,26 +16,27 @@
     <header class="header">
         <div class="container-fluid pt-0">
             <div class="row">
-                <nav class="navbar navbar-left col-lg-6">
-                    <ul class="p-0">
+                <nav class="navbar navbar-left col-lg-7">
+                    <ul class="m-0">
                         <li><a href="#">
                                 <img src="/img/logo_pdf2go.svg" alt="logo">
                             </a></li>
-                        <li><a href="#"> Все инструменты </a></li>
+                        <li><a href="#" class="navbar__bordered"><i class="fas fa-th-large"></i> Все инструменты
+                                <i class="fas fa-angle-down"></i></a></li>
                         <li><a href="#">Редактировать PDF</a></li>
                         <li><a href="#">Сжать PDF</a></li>
                         <li><a href="#">Разделить PDF</a></li>
                         <li><a href="#">Объединить PDF</a></li>
                     </ul>
                 </nav>
-                <nav class="navbar navbar-right offset-lg-2 col-lg-4">
-                    <ul class="p-0">
+                <nav class="navbar navbar-right offset-lg-1 col-lg-4">
+                    <ul class="m-0">
                         <li><a href="#">Купить</a></li>
                         <li><a href="#">Справка</a></li>
                         <li><a href="#">PDF2Go</a></li>
-                        <li><a href="#"><i class="fas fa-globe"></i>RU</a></li>
-                        <li><a href="#">Вход</a></li>
-                        <li><a href="#">Регистрация</a></li>
+                        <li><a href="#" class="navbar__bordered">RU <i class="fas fa-globe"></i></a></li>
+                        <li><a href="#"><i class="fas fa-sign-in-alt"></i> Вход</a></li>
+                        <li><a href="#" class="btn btn-success navbar__register">Регистрация</a></li>
                     </ul>
                 </nav>
             </div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,16 +5,32 @@
 {{-- Main Content --}}
 
 <main class="main-content">
-    <div class="container">
+    <div class="container-md">
+        <div class="row">
+            <div class="col">
+                <div class="main-content__header">
+                    <h1>Редактировать PDF</h1>
 
+                    <h2>Онлайн-редактор позволяет редактировать PDF-файлы. Вы можете добавить текст,
+                        вставить изображение, нарисовать прямоугольники,
+                        круги и стрелки, выделить абзац, вырезать и скопировать часть документа.</h2>
+                </div>
 
-        <div class="row row-cols-5 justify-content-center">
-            <div class="col"><p><a href="">Column</a></p></div>
-            <div class="col"><p><a href="">Column</a></p></div>
-            <div class="col"><p><a href="">Column</a></p></div>
-            <div class="col"><p><a href="">Column</a></p></div>
-            <div class="col"><p><a href="">Column</a></p></div>
-            <div class="col"><p><a href="">Column</a></p></div>
+                <div class="main-content__outterbox">
+                    <div class="main-content__innerbox d-flex align-items-center flex-column">
+                        <div class="main-content__icon">
+                            <i class="fas fa-10x fa-cloud-upload-alt" style="color:gray;"></i>
+                            
+                            </div>
+                            <p><span>Перетащите файлы сюда</span></p>
+                            <div>
+                                <button class="btn btn-primary px-5 py-3" title="Загрузить файл с устройства"><i class="fas fa-search"></i>
+                                    Выберите файл
+                                </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
